021096751b051d51184396bdf4d8aae052b7e070
benjamin.weatherill <benjamin.weatherill@mymail.champlain.edu>
2022-02-03 23:55:07 -0500

    Added testing div

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function testFunc() {
-	document.getElementById('test').innerHTML = Date()
+	document.getElementById('test').innerHTML = Date();
+}
+
+function switchHidden() {
+	let x = document.getElementById('myButton');
+	
+	if (x.style.visibility == "visible") {
+		x.style.visibility = "hidden";
+	}
+	else {
+		x.style.visibility = "visible";
+	}
 }
