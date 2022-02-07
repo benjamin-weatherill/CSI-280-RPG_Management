2902ee6622e1248d23c426045bad8177eab26121
justinwillis145 <72421206+justinwillis145@users.noreply.github.com>
2022-02-07 18:48:10 -0500

    Update RNG rpg.js

--- RNG rpg.js
+++ RNG rpg.js
@@ -1,8 +1,8 @@
 //Random Number Generator for RPG
 function rng(min, max){
-    let min = "";
-    let max = "";
-    let itemDrop = "";
+    let min = 0; //Should be set to zero
+    let max = ""; //The higher the maximum the rarer the item, can be interchanged
+    let itemDrop = ""; //item drop should be constant among all items if this equals the math.random number the item should drop
     if(itemDrop == Math.random(min, max)){
         return itemDrop;
     }
