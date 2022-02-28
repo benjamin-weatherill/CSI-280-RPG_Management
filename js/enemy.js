17221c3cfbaad105a4bba57a96a61b7e95ce0a48
benjamin.weatherill <benjamin.weatherill@mymail.champlain.edu>
2022-02-28 18:09:51 -0500

    Moved enemy.js

---
+++ js/enemy.js
@@ -0,0 +1,7 @@
+//Enemy tracker, we can change this as needed
+function enemyClass(enemyHp, enemyAttack, enemyExpDrop, enemyDefense){
+    let enemyName = "";
+    let enemyAttack = 0; // chanve as needed
+    let enemyDefense = 0; //change as needed
+    let enemyExpDrop = 0; //change as needed
+}
