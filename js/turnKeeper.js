40285598ef13a8813c8bdc8d919008a4ffaa572d
DBStrats <andrew.barnett@mymail.champlain.edu>
2022-03-03 17:06:36 -0500

    Added a possible NPC slot for turn (should allies be a thing). Also, creatures stats can be saved/accessed as part of the creature.

--- js/turnKeeper.js
+++ js/turnKeeper.js
@@ -1,24 +1,34 @@
 /*
+playerID = 0;
+enemyID = 1;
+NPC_ID = 2;
+
 class Creature{
-    constructor(creatureID, creatureName){
+    constructor(creatureID, creatureName, creatureStats){
 	this.creatureID = creatureID;
-	this.creatureName = creatureName
+	this.creatureName = creatureName;
+	this.creatureStats = creatureStats;
     }
 }
 
 
 const combatants = [];
+
+combatants = turnOrder();
+
 for (let x = 0; x < combatants.length; x++){
     text += combatants[x].creatureName + "'s turn: Take your action!"
     actionList(combatants[x]);
 }
 
 function actionList(creature){
-    if (creature.creatureID == 0){
-	text += "Fight!\nItems\nMove\n"
+    if (creature.creatureID == playerID){
+	text += "Fight!\nItems\nMove\n";
     }
-    else if (creature.creatureID == 1){
-	text += "Fight!\nMove\n"
+    else if (creature.creatureID == enemyID){
+	text += "Fight!\nMove\n";
     }
+    else if (creature.creatureID == NPC_ID)
+	text += "Fight!\nAssist\nMove\n";
 }
 */
