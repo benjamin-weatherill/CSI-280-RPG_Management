23c58f54ed435c8dee45d97ba3e25daf91ba58fb
benjamin.weatherill <benjamin.weatherill@mymail.champlain.edu>
2022-03-03 16:55:35 -0500

    Changed download button

--- js/backend_dev.js
+++ js/backend_dev.js
@@ -3,4 +3,11 @@ function download(data, name) {
     let blob = new Blob([data], {type: "text/plain"});
     a.href = URL.createObjectURL(blob);
     a.download = name;
+    a.style.visibility = "visible";
+    document.getElementById("saveButton").style.visibility = "hidden";
+}
+
+function revertDownload() {
+    document.getElementById("a").style.visibility = "hidden";
+    document.getElementById("saveButton").style.visibility = "visible";
 }
