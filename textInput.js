1beafd472d42164baee7b8a9ebe58003b00809b3
justinwillis145 <72421206+justinwillis145@users.noreply.github.com>
2022-03-07 19:02:22 -0500

    Text input from user
    Needs to be changed to fit our needs, just a basic file for now.

---
+++ textInput.js
@@ -0,0 +1,21 @@
+//Text input from the User
+document.getElementById("userInput").onload = function() {userInput}
+
+function userInput(input){
+    let input = window.prompt("Select your character: Knight, Mage, Cleric, or Theif.");
+    if(input == "Knight" || input == "knight"){
+        alert("You chose the class Knight!");
+    }
+    else if (input == "Mage" || input == "mage"){
+        alert("You chose the class Mage!");
+    }
+    else if (input == "Cleric" || input == "cleric"){
+        alert("You chose the class Cleric!");
+    }
+    else if (input == "Theif" || input == "theif"){
+        alert("You chose the class Theif!");
+    }
+    else{
+        input = window.prompt("Please choose one of the four Characters: Knight, Mage, Cleric, or Theif." );
+    }
+}
